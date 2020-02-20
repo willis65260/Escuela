982bf18a79ec24062fc38c10cc612f0a3fd7fc69
willis65260 <48706135+willis65260@users.noreply.github.com>
2020-02-20 12:05:38 -0600

    Bases de datios fragmentadas

--- Arquitectura de Computadoras/sketch_feb13a/sketch_feb13a.ino
+++ Arquitectura de Computadoras/sketch_feb13a/sketch_feb13a.ino
@@ -1,4 +1,27 @@
-int numero=100; long resultado;
-void setup() {
-Serial.begin(9600); resultado=long(numero)*1000; Serial.println(resultado);
-} void loop() {}
+const int LED=13;
+const int BOTON=10;
+
+int val=0;
+int state=0;
+int old_val=0;
+
+void setup(){
+  
+  pinMode(LED,OUTPUT);
+  pinMode(BOTON,INPUT);
+  
+}
+void loop(){
+  val=digitalRead(BOTON);
+  if((val==HIGH) && (old_val==LOW)){
+    state=1-state;
+    //delay(10);
+  }
+  old_val=val;
+  if(state==1){
+   digitalWrite(LED,HIGH); 
+  }else{
+    digitalWrite(LED,LOW);
+  }
+  
+}
