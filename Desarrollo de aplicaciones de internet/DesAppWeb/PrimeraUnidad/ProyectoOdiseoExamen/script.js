caeb7f3ab95a4c3f04926c98bb95a546796c3adb
willis65260 <48706135+willis65260@users.noreply.github.com>
2020-03-11 23:23:10 -0600

    Proyecto Examen Automatas

--- Desarrollo de aplicaciones de internet/DesAppWeb/PrimeraUnidad/ProyectoOdiseoExamen/script.js
+++ Desarrollo de aplicaciones de internet/DesAppWeb/PrimeraUnidad/ProyectoOdiseoExamen/script.js
@@ -6,17 +6,17 @@ var aux = false;
 var aux2 = false;
 
 
-boton_alfabeto.onclick = function() {
-    var text_alfabeto = document.getElementById("text-alfabeto").value;
-    var text_estados = document.getElementById("text-estados").value;
-    if (text_alfabeto == "") {
-        alert("El alfabeto no puede estar vacio");
-    } else if (text_estados == "") {
-        alert("La cantidad de estados no puede estar vacia");
-    } else {
-        Llenar_State_Table(text_alfabeto, text_estados);
-    }
-}
+// boton_alfabeto.onclick = function() {
+//     var text_alfabeto = document.getElementById("text-alfabeto").value;
+//     var text_estados = document.getElementById("text-estados").value;
+//     if (text_alfabeto == "") {
+//         alert("El alfabeto no puede estar vacio");
+//     } else if (text_estados == "") {
+//         alert("La cantidad de estados no puede estar vacia");
+//     } else {
+//         Llenar_State_Table(text_alfabeto, text_estados);
+//     }
+// }
 
 function validar_solo_numeros(e) {
 
@@ -60,11 +60,9 @@ function generar_select(estados, cadena) {
     for (let i = 0; i < selecs.length; i++) {
         var sel = document.createElement("SELECT");
         var nombre = "sel" + i;
-        console.log("nombre: " + nombre);
+        // console.log("nombre: " + nombre);
         sel.setAttribute("id", nombre);
         selecs[i] = (sel);
-
-
         //Le añado como estado inicial, un estado de error 
         var z = document.createElement("option");
         var nom = "E";
@@ -78,7 +76,7 @@ function generar_select(estados, cadena) {
             var letra = cadena.charAt(j);
             var z = document.createElement("option");
             var nom = "Q" + j;
-            console.log("nom: " + nom);
+            // console.log("nom: " + nom);
             z.setAttribute("value", nom);
             var t = document.createTextNode(nom);
             z.appendChild(t);
@@ -110,8 +108,8 @@ function generar_filas(cadena, estados, alfabeto) {
     var acepts = generar_check(estados);
     final_selecs = selecs;
     final_acepts = acepts;
-    console.log(selecs);
-    console.log(acepts);
+    // console.log(selecs);
+    // console.log(acepts);
     for (let j = 0, cont = 0; j < parseInt(estados); j++) {
         cadena = cadena + "<tr> <td>Q" + j + "</td>";
         for (let i = 0; i < alfabeto.length; i++) {
@@ -146,44 +144,249 @@ function Llenar_State_Table(text_alfabeto, text_estados) {
     aux = true;
 }
 
-boton_tabla.onclick = function() {
-    if (aux) {
-        var cad_val = document.getElementById("cad-val");
-        cad_val.style.display = "flex";
-        aux2 = true;
-    }
-}
+// boton_tabla.onclick = function() {
+//     if (aux) {
+//         var cad_val = document.getElementById("cad-val");
+//         cad_val.style.display = "flex";
+//         aux2 = true;
+//     }
+// }
 
 function Llenar_Alfabeto() {
     /*Llenar El alfabeto con el codigo ascci de 
     0-256 exceptuando la cadena Vacia*/
-
-
-
 }
 
 var final_alfabeto = "";
-var final_estados = 19;
+var final_estados = 18;
 var final_selecs = [];
 var final_acepts = [];
 var car_act = "";
 var val_car = 0;
 var salto = 0;
+var c_c = new Array(256);
+var cc_texto = "";
+
+function Acomodar_Checks() {
+    document.getElementById("AQ" + 1).checked = 1;
+    document.getElementById("AQ" + 3).checked = 1;
+    document.getElementById("AQ" + 5).checked = 1;
+    document.getElementById("AQ" + 7).checked = 1;
+    document.getElementById("AQ" + 10).checked = 1;
+    document.getElementById("AQ" + 11).checked = 1;
+    document.getElementById("AQ" + 14).checked = 1;
+    document.getElementById("AQ" + 17).checked = 1;
+}
+
+function Acomodar_Selecs() {
+
+    for (let s = 0; s < final_estados; s++) {
+        for (let vc = 0; vc < final_alfabeto.length; vc++) {
+            var andrea = (s * (final_selecs.length / final_estados));
+            var v2 = vc + andrea;
+            if (s == 0) {
+                if (v2 == 48 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 2;
+                }
+                if (v2 == 45 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 5;
+                }
+                if (v2 >= 49 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 6;
+                }
+                if (v2 == 95 + andrea || (v2 >= 65 + andrea && v2 <= 122 + andrea)) {
+                    document.getElementById("sel" + v2).selectedIndex = 12;
+                }
+                if (v2 == 39 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 13;
+                }
+                if (v2 == 34 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 16;
+                }
+            }
+
+            if (s == 1) {
+                if (v2 == 46 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 3;
+                }
+            }
+
+            if (s == 2) {
+                if (v2 == 48 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 4;
+                }
+                if (v2 >= 49 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 6;
+                }
+            }
+
+            if (s == 3) {
+                if (v2 >= 48 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 6;
+                }
+            }
+
+            if (s == 4) {
+                if (v2 >= 49 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 8;
+                }
+            }
+
+            if (s == 5) {
+                if (v2 >= 48 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 6;
+                }
+                if (v2 == 46 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 7;
+                }
+            }
+
+            if (s == 6) {
+                if (v2 == 48 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 7;
+                }
+                if (v2 >= 49 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 8;
+                }
+            }
+
+            if (s == 7) {
+                if (v2 == 101 + andrea || v2 == 69 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 9;
+                }
+            }
+
+            if (s == 8) {
+                if (v2 == 45 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 10;
+                }
+                if (v2 >= 49 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 11;
+                }
+            }
+
+            if (s == 9) {
+                if (v2 >= 49 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 11;
+                }
+            }
+
+            if (s == 10) {
+                if (v2 >= 48 + andrea && v2 <= 57 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 11;
+                }
+            }
+
+            if (s == 11) {
+                if (v2 == 95 + andrea || (v2 >= 65 + andrea && v2 <= 122 + andrea) || (v2 >= 48 + andrea && v2 <= 57 + andrea)) {
+                    document.getElementById("sel" + v2).selectedIndex = 12;
+                }
+            }
+
+            if (s == 12) {
+                if ((v2 >= 0 + andrea && v2 <= 38 + andrea) || (v2 >= 40 + andrea && v2 <= 256 + andrea)) {
+                    document.getElementById("sel" + v2).selectedIndex = 14;
+                }
+                if (v2 == 39 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 15;
+                }
+            }
+
+            if (s == 13) {
+                if (v2 == 39 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 15;
+                }
+            }
+
+            if (s == 15) {
+                if ((v2 >= 0 + andrea && v2 <= 33 + andrea) || (v2 >= 35 + andrea && v2 <= 256 + andrea)) {
+                    document.getElementById("sel" + v2).selectedIndex = 17;
+                }
+                if (v2 == 34 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 18;
+                }
+            }
+
+            if (s == 16) {
+                if ((v2 >= 0 + andrea && v2 <= 33 + andrea) || (v2 >= 35 + andrea && v2 <= 256 + andrea)) {
+                    document.getElementById("sel" + v2).selectedIndex = 17;
+                }
+                if (v2 == 34 + andrea) {
+                    document.getElementById("sel" + v2).selectedIndex = 18;
+                }
+            }
+
+        }
+    }
+}
+
+function Separar_por_espacios_en_blanco(ceneval) {
+    var cads = new Array();
+    var cad_aux = "";
+    console.log(ceneval);
+    // console.log(cad_val.charCodeAt(''));
+    for (var i = 0; i < ceneval.length; i++) {
+        // if (cad_val.charAt(i) != " ") {
+        //     cad_aux = cad_aux + cad_val.charAt(i);
+        // } else {
+        //     cads.push(cad_aux);
+        //     console.log(cad_aux);
+        //     cad_aux = "";
+        // }
+        if (ceneval.charAt(i) == " ") {
+            cads.push(cad_aux);
+            console.log(cad_aux);
+            cad_aux = "";
+        } else {
+            cad_aux = cad_aux + ceneval.charAt(i);
+        }
+    }
+    cads.push(cad_aux);
+    console.log(cad_aux);
+    return cads;
+}
 
 function validacion_super_mamalona_estoy_mamadisimo_hdp() {
     var cad_val = document.getElementById("cad-vali").value;
-    for (let a = 0; a < final_alfabeto.length; a++) {
-        for (let an = 0; an < cad_val.length; an++) {
-            console.log("cad_val: " + cad_val.charAt(an));
-            console.log("final_alfabeto: " + final_alfabeto.charAt(a));
-            if ((final_alfabeto.indexOf(cad_val.charAt(an))) != -1) {
-                //Todo Bien krnal
-            } else {
-                alert("la cadena contiene caracteres que no se encuentran en el alfabeto");
-                return;
-            }
-        }
+    // for (let a = 0; a < final_alfabeto.length; a++) {
+    //     for (let an = 0; an < cad_val.length; an++) {
+    //         console.log("cad_val: " + cad_val.charAt(an));
+    //         console.log("final_alfabeto: " + final_alfabeto.charAt(a));
+    //         if ((final_alfabeto.indexOf(cad_val.charAt(an))) != -1) {
+    //             //Todo Bien krnal
+    //         } else {
+    //             alert("la cadena contiene caracteres que no se encuentran en el alfabeto");
+    //             return;
+    //         }
+    //     }
+    // }
+
+    // console.log("Hola");
+
+    // var cadenas = Separar_por_espacios_en_blanco(cade);
+    // console.log(cadenas + " longitud " + cadenas.length);
+    // for (var k = 0; k < cadenas.length; k++) {
+    //     var cad_val = "";
+
+    //     console.log(cadenas[k]);
+
+    //     cad_val = cadenas.indexOf(k);
+    //     console.log(cad_val);
+
+
+    for (var c_caracter = 0; c_caracter < c_c.length; c_caracter++) {
+        c_c[c_caracter] = String.fromCharCode(c_caracter);
+        cc_texto = cc_texto + "" + String.fromCharCode(c_caracter);
+        // console.log(c_c[c_caracter]);
     }
+    final_alfabeto = cc_texto;
+
+    // console.log(final_alfabeto);
+    Llenar_State_Table(final_alfabeto, final_estados);
+    Acomodar_Selecs();
+    Acomodar_Checks();
+
+    // console.log(cc_texto);
     var cad_val = document.getElementById("cad-vali").value;
     for (var i = 0; i < cad_val.length; i++) {
         car_act = cad_val.charAt(i);
@@ -195,39 +398,60 @@ function validacion_super_mamalona_estoy_mamadisimo_hdp() {
         }
         var andrea = (salto * (final_selecs.length / final_estados));
         val_car = val_car + andrea;
-        console.log("val_car: " + val_car);
-        console.log("andrea: " + andrea);
+        // console.log("val_car: " + val_car);
+        // console.log("andrea: " + andrea);
         sel_actual = document.getElementById("sel" + val_car);
-        console.log("sel" + val_car);
-        var value = sel_actual[sel_actual.selectedIndex].value;
+        // console.log("sel" + val_car);
+        // console.log(sel_actual);
+        // console.log(final_alfabeto);
+
+        var value = sel_actual.value;
 
         for (var j = 0; j < final_estados; j++) {
-            console.log(value);
-            console.log("Q" + j)
+            // console.log(value);
+            // console.log("Q" + j)
             if (value == "Q" + j) {
                 salto = j;
-                console.log("Salto: " + salto);
+                // console.log("Salto: " + salto);
                 break;
             } else if (value == "E") {
                 salto = j;
-                alert("Cadena Invalida");
+                // console.log(salto);
+                alert("Cadena Invalida 1 " + cad_val + " salto " + salto);
                 limpiar_puerquero();
                 return;
             }
         }
     }
-    console.log("Llego al final de la cadena");
+    // console.log("Llego al final de la cadena");
     var acep = document.getElementById("AQ" + salto);
+    var mensaje = "";
     if (acep.checked) {
-        alert("Cadena Valida");
+        if (salto == 1 || salto == 5) {
+            mensaje = "Entero";
+            if (cad_val.indexOf('.')) {
+                mensaje = "Flotante";
+            }
+        } else if (salto == 3 || salto == 7 || salto == 10) {
+            mensaje = "Flotante";
+        } else if (salto == 11) {
+            mensaje = "Identificador";
+        } else if (salto == 14) {
+            mensaje = "Char";
+        } else if (salto == 17) {
+            mensaje = "Cadena";
+        }
+        alert("Cadena Valida es un/a " + mensaje + " " + cad_val);
         limpiar_puerquero();
         return;
     } else {
-        alert("Cadena Invalida");
+        alert("Cadena Invalida 2 " + cad_val + " " + salto);
         limpiar_puerquero();
         return;
     }
+
 }
+// }
 
 function limpiar_puerquero() {
     car_act = "";
@@ -235,7 +459,7 @@ function limpiar_puerquero() {
     salto = 0;
 }
 boton_cad.onclick = function() {
-    if (aux2) {
-        validacion_super_mamalona_estoy_mamadisimo_hdp();
-    }
+    // if (aux2) {
+    validacion_super_mamalona_estoy_mamadisimo_hdp();
+    // }
 }
